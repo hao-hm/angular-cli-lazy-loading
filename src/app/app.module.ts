51f0a7a046fc5c20bed9e51771255db71b271386
Hao Huynh <minhhao411@gmail.com>
2016-12-20 23:26:16 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { DashboardModule } from './feature/dashboard/dashboard.module'
+// import { UserModule } from './feature/user/user.module'
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DashboardModule,
+    // UserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
